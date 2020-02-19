bc6afe0bbb97dfb38519d4dec475ddd46968bf7e
John J Marshall Jr <johnjamesmarshalljr@gmail.com>
2020-02-19 20:42:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,7 +14,11 @@ def long_planeteer_calls(arr)# code an argument here
  arr.any? {|el| el.length > 4  }  # Your code here
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(arr)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+arr.select do |str|
+  cheese_types.include?(str)
+end
+
 end
